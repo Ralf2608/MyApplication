ce56c333636ed5001b7b22bba43e3f6b5efc4ec7
Rafael Santana <ralfsantanasantos@gmail.com>
2022-06-23 20:13:17 -0300

    incrementação do jokenpô nos jogos

--- Jogos/jogos.py
+++ Jogos/jogos.py
@@ -1,10 +1,11 @@
 import forca
 import adivinhacao
+import jokenpo
 
 def escolha_jogo():
     print("Escolha o seu jogo: ")
 
-    print("(1) Forca\n(2) Adivinhação")
+    print("(1) Forca\n(2) Adivinhação\n(3) Jokenpô")
 
     jogo = int(input("Qual o jogo?\n"))
 
@@ -14,6 +15,9 @@ def escolha_jogo():
     elif(jogo == 2):
         print("Jogando adivinhação!")
         adivinhacao.jogar()
+    elif(jogo == 3):
+        print('Jogando Jokenpô')
+        jokenpo.jogar()
 
 if(__name__ == "__main__"):
     escolha_jogo()
